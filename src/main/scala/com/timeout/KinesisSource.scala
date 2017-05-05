a422f59fcb2fc467b5a71955bc550affbb082e52
Tom Verran <tomverran@timeout.com>
2017-05-05 18:14:03 +0100

    [wip] handle more backpressure
    To allow the stream to backpressure for more time than the life
    of a shard iterator I'm going to pair every iterator with a request
    that'll reissue itself from the current point in the stream
    
    This is untested and uncompiled, will be continued next week

--- src/main/scala/com/timeout/KinesisSource.scala
+++ src/main/scala/com/timeout/KinesisSource.scala
@@ -1,7 +1,7 @@
 package com.timeout
 
 import java.nio.ByteBuffer
-import java.time.{Clock, ZonedDateTime}
+import java.time.{Clock, ZonedDateTime, Duration => JDuration}
 import java.util.Date
 import java.util.concurrent.{Future => JavaFuture}
 
@@ -11,13 +11,13 @@ import akka.stream.stage.{GraphStage, OutHandler, StageLogging, TimerGraphStageL
 import akka.stream.{Attributes, Outlet, SourceShape}
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.kinesis.model._
-import com.amazonaws.services.kinesis.{AmazonKinesisAsync, AmazonKinesisAsyncClientBuilder}
+import com.amazonaws.services.kinesis.{AmazonKinesis, AmazonKinesisAsync, AmazonKinesisAsyncClientBuilder}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object KinesisSource {
 
@@ -28,6 +28,35 @@ object KinesisSource {
 
   private[timeout] lazy val kinesis: AmazonKinesisAsync =
     AmazonKinesisAsyncClientBuilder.standard.withRegion(region).build
+
+  private [timeout] case class ShardIterator(
+    iterator: String,
+    reissue: GetShardIteratorRequest
+  )
+
+  /**
+    * Given a response from kinesis and the current shard iterator, prepare a new iterator.
+    * The new iterator needs to have a reissue command that can reproduce it if it expires.
+    * To do this we either use an AFTER_SEQUENCE_NUMBER request if we have records, or
+    * an AT_TIMESTAMP request if we don't
+    */
+  private [timeout] def nextIterator(
+    s: ShardIterator,
+    g: GetRecordsResult
+  )(implicit c: Clock): ShardIterator = {
+    val reissue = g.getRecords.asScala.lastOption.fold {
+      val nanosBehind = g.getMillisBehindLatest * 1000
+      val ts = ZonedDateTime.now(c).minusNanos(nanosBehind)
+      s.reissue
+        .withShardIteratorType("AT_TIMESTAMP")
+        .withTimestamp(Date.from(ts.toInstant))
+    } { lastRecord =>
+      s.reissue
+        .withShardIteratorType("AFTER_SEQUENCE_NUMBER")
+        .withStartingSequenceNumber(lastRecord.getSequenceNumber)
+    }
+    ShardIterator(g.getNextShardIterator, reissue)
+  }
 
   /**
     * This creates a source that reads records from AWS Kinesis.
@@ -86,18 +115,17 @@ private[timeout] class KinesisSource(
   override def createLogic(inheritedAttributes: Attributes) = new TimerGraphStageLogic(shape) with StageLogging {
 
     // stores futures from kinesis get records requests
-    val buffer = mutable.Map.empty[String, JavaFuture[GetRecordsResult]]
+    val buffer = mutable.Map.empty[ShardIterator, JavaFuture[GetRecordsResult]]
 
     Future {
       val stream = kinesis.describeStream(streamName).getStreamDescription
       shardIteratorRequests(since, stream).toStream.par
-        .map(kinesis.getShardIterator)
-        .map { i =>
-          val iterator = i.getShardIterator
-          val request = new GetRecordsRequest().withShardIterator(iterator)
-          iterator -> kinesis.getRecordsAsync(request)
+        .map { shardReq =>
+          val i = kinesis.getShardIterator(shardReq).getShardIterator
+          val request = new GetRecordsRequest().withShardIterator(i)
+          ShardIterator(i, shardReq) -> kinesis.getRecordsAsync(request)
         }.toMap.seq
-    }.onComplete(getAsyncCallback[Try[Map[String, JavaFuture[GetRecordsResult]]]] { iterators =>
+    }.onComplete(getAsyncCallback[Try[Map[ShardIterator, JavaFuture[GetRecordsResult]]]] { iterators =>
       val unsafeIterators = iterators.get // trigger an exception if we could not bootstrap
       unsafeIterators.foreach(buffer += _)
     }.invoke(_))
@@ -113,18 +141,43 @@ private[timeout] class KinesisSource(
     override def beforePreStart() =
       schedulePeriodically("kinesis", 100.millis)
 
+    def emitRecords(currentIterator: ShardIterator, result: GetRecordsResult): Unit = {
+      val newIterator = nextIterator(currentIterator, result)
+      val nextRequest = new GetRecordsRequest().withShardIterator(newIterator.iterator)
+      val newFuture = newIterator -> kinesis.getRecordsAsync(nextRequest)
+      emitMultiple[ByteBuffer](outlet, result.getRecords.asScala.map(_.getData).toList, { () =>
+        buffer += newFuture
+      })
+    }
+
+    def reissueIterator(iterator: ShardIterator): Unit = {
+      Future {
+        kinesis.getShardIterator(iterator.reissue)
+      }.map { result =>
+        val newIt = iterator.copy(iterator = result.getShardIterator)
+        val req = new GetRecordsRequest().withShardIterator(newIt.iterator)
+        newIt -> kinesis.getRecordsAsync(req)
+      }.onComplete(getAsyncCallback[Try[(ShardIterator, JavaFuture[GetRecordsResult])]] { r =>
+        buffer += r.get // again, forcing akka to catch exceptions
+      }.invoke(_))
+    }
+
+    def retryRequest(iterator: ShardIterator): Unit = {
+      val request = new GetRecordsRequest().withShardIterator(iterator.iterator)
+      buffer += (iterator -> kinesis.getRecordsAsync(request))
+    }
+
     def tryToEmitRecords() = {
       buffer.filter(_._2.isDone).foreach { case (iterator, future) =>
         buffer.remove(iterator)
-        val result = Try(future.get)
-        val newIterator = result.toOption.fold(iterator)(_.getNextShardIterator)
-        val newFuture = newIterator -> kinesis.getRecordsAsync(new GetRecordsRequest().withShardIterator(newIterator))
 
-        log.debug(s"Emitting ${result.toOption.map(_.getRecords.size).getOrElse(0)} records...")
-        emitMultiple(outlet, result.toOption.toList.flatMap(_.getRecords.asScala.map(_.getData).toList), { () =>
-          buffer += newFuture
-          ()
-        })
+        Try(future.get) match {
+          case Success(recordsResult) => emitRecords(iterator, recordsResult)
+          case Failure(ExpiredIteratorException) => reissueIterator(iterator)
+          case Failure(error) =>
+            log.debug(error.getMessage)
+            retryRequest(iterator)
+        }
       }
     }
   }
